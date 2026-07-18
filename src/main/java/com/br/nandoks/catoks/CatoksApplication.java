4ba0aace4940ab32a75b15093242848b48705238
nandoks <fernandooksman@gmail.com>
2026-07-18 16:03:09 +0200

    Kicktart project

---
+++ src/main/java/com/br/nandoks/catoks/CatoksApplication.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.catoks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatoksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatoksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/nandoks/catoks/CatoksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.catoks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatoksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
